f85c6e58c304927ee247a035fbd02282f3d97bd0
carherval <carlos.hernandezvalero@gmail.com>
2025-02-10 20:28:36 +0100

    Versión final

--- simon/main.js
+++ simon/main.js
@@ -1,5 +1,37 @@
 import './style.css'
 
+import { createHeaderTag } from './src/components/header/header'
+
+import {
+  STAT_LABELS,
+  getButtonLink,
+  getHiddenButtonLink,
+  getStatsByProperty,
+  deleteStats,
+  saveStats
+} from './src/components/game/stats/stats'
+
+import {
+  BOARD_COLORS,
+  colorSequence,
+  getGameBoardContent,
+  createColorSequence,
+  isCorrectColor,
+  resetColorSequence
+} from './src/components/game/board/board'
+
+import {
+  getRadioButtonInputGroup,
+  getSubmitButton
+} from './src/components/game/config/config'
+
+import {
+  getGameMessageContent,
+  printMessage
+} from './src/components/game/messages/messages'
+
+import { createFooterTag } from './src/components/footer/footer'
+
 // Mensajes emergentes de SweetAlert2
 import Swal from 'sweetalert2'
 
@@ -9,6 +41,25 @@ const FOOTER_HEIGHT = 7.5
 const GAME_TITLE = 'Simon'
 const NEW_GAME_LABEL = 'Nueva partida'
 const NOT_STARTED_GAME_MSG = 'Partida no iniciada'
+const HIT_MESSAGE = 'Número máximo de aciertos'
+
+const DIFF_ID = 'diff'
+const ACTIVE_CLASS_PREFIX = 'active-'
+
+const MAX_SEQ = 50
+
+// Cabeceras de las tablas de las estadísticas del juego
+const STAT_HEADERS = {
+  date: { id: 'date', title: 'Fecha' },
+  hits: { id: 'hits', title: 'Aciertos' }
+}
+
+// Dificultades del juego
+const DIFFICULTIES = {
+  easy: { id: 'easy', title: 'Fácil', delay: 1000 },
+  medium: { id: 'medium', title: 'Media', delay: 500 },
+  hard: { id: 'hard', title: 'Difícil', delay: 250 }
+}
 
 // Etiquetas para los mensajes emergentes
 const DIALOG_LABELS = {
@@ -16,11 +67,415 @@ const DIALOG_LABELS = {
   cancel: 'Cancelar'
 }
 
+// Dificultad del juego
+let gameDifficulty = DIFFICULTIES.medium.id
+// Indicador de partida iniciada
+let isStartedGame = false
+// Indicador de secuencia activa (reproducción)
+let isActiveSequence = false
+// Contador de la posición de la secuencia de colores de la partida
+let colorSequencePos = 0
+// Contador de la posición de la secuencia de colores del jugador
+let playerSequencePos = 0
+// Resultado del juego
+let gameHits = 0
+
+createHeaderTag(GAME_TITLE, HEADER_HEIGHT)
+createMainTag()
+createFooterTag(FOOTER_HEIGHT)
+
+// Función que crea la zona de juego
+function createMainTag() {
+  const main = document.querySelector('main')
+
+  main.classList.add('flex', 'game')
+  main.style = `height: calc(100vh - ${
+    HEADER_HEIGHT + FOOTER_HEIGHT
+  }rem); overflow-y: auto;`
+
+  main.innerHTML =
+    getGameStatContent() +
+    getGameConfigAndBoardContent() +
+    getGameMessageContent()
+
+  document.getElementById(gameDifficulty).checked = true
+
+  addMainTagListeners()
+  printStats()
+  printMessage(NOT_STARTED_GAME_MSG)
+}
+
+/**
+ * Función que devuelve el contenido de las estadísticas del juego
+ * @returns Código HTML
+ */
+function getGameStatContent() {
+  return `${getStatButtonBoxContent()}${getStatTableContent()}`
+}
+
+/**
+ * Función que devuelve el contenido de la botonera de las estadísticas del juego
+ * @returns Código HTML
+ */
+function getStatButtonBoxContent() {
+  return `<div class="flex">${getButtonLink(
+    STAT_LABELS.view.id,
+    STAT_LABELS.view.title
+  )}${getHiddenButtonLink(
+    STAT_LABELS.back.id,
+    STAT_LABELS.back.title
+  )}${getHiddenButtonLink(STAT_LABELS.del.id, STAT_LABELS.del.title)}</div>`
+}
+
+/**
+ * Función que devuelve el contenido de las tablas de las estadísticas del juego
+ * @returns Código HTML
+ */
+function getStatTableContent() {
+  let statTables = '<section class="flex stats oculto">'
+
+  for (const difficulty in DIFFICULTIES) {
+    statTables += `<article class="flex" id="${DIFF_ID}-${difficulty}"><table summary="Estadísticas almacenadas de las partidas para la dificultad ${DIFFICULTIES[
+      difficulty
+    ].title.toLowerCase()}"><caption><h2>Dificultad ${DIFFICULTIES[
+      difficulty
+    ].title.toLowerCase()}</h2><span></span></caption><thead>${getStatHeaderRow(
+      difficulty[0]
+    )}</thead><tbody>${getNoStatsRow(difficulty[0])}</tbody></table></article>`
+  }
+
+  statTables += '</section>'
+
+  return statTables
+}
+
+/**
+ * Función que devuelve la cabecera de las tablas de las estadísticas del juego
+ * @param {String} idPrefix Prefijo para el identificador de las cabeceras
+ * @returns Código HTML
+ */
+function getStatHeaderRow(idPrefix) {
+  let statHeaderRow = '<tr>'
+
+  for (const statHeader in STAT_HEADERS) {
+    statHeaderRow += `<th id="${idPrefix}-${statHeader}" scope="col">${STAT_HEADERS[statHeader].title}</th>`
+  }
+
+  statHeaderRow += '</tr>'
+
+  return statHeaderRow
+}
+
+/**
+ * Función que devuelve una fila vacía para las tablas de las estadísticas del juego
+ * @param {String} idPrefix Prefijo para el identificador de las cabeceras
+ * @returns Código HTML
+ */
+function getNoStatsRow(idPrefix) {
+  let statNoInfoRow = `<tr><td colspan="${
+    Object.keys(STAT_HEADERS).length
+  }" headers="`
+
+  for (const statHeader in STAT_HEADERS) {
+    statNoInfoRow += `${idPrefix}-${statHeader} `
+  }
+  // Se elimina el espacio final
+  statNoInfoRow = statNoInfoRow.slice(0, -1)
+
+  statNoInfoRow += '">No hay información disponible</td></tr>'
+
+  return statNoInfoRow
+}
+
+/**
+ * Función que devuelve las opciones de configuración y el tablero de juego
+ * @returns Código HTML
+ */
+function getGameConfigAndBoardContent() {
+  return `<section class="flex config-board">${getGameConfigContent()}${getGameBoardContent()}</section>`
+}
+
+/**
+ * Función que devuelve las opciones de configuración del juego
+ * @returns Código HTML
+ */
+function getGameConfigContent() {
+  return `<div class="flex config"><h2 class="oculto">Configuración del juego</h2><form class="flex" action="#" method="post" onsubmit="javascript:return false">
+  <fieldset class="flex"><legend>Selecciona la dificultad:</legend><div class="flex">${getRadioButtonInputGroup(
+    DIFFICULTIES,
+    DIFF_ID
+  )}</div></fieldset><fieldset class="flex" id="submit"><legend class="oculto">${NEW_GAME_LABEL}</legend>${getSubmitButton(
+    NEW_GAME_LABEL
+  )}</fieldset></form></div>`
+}
+
+// Función que crea los eventos de la zona de juego
+function addMainTagListeners() {
+  addGameStatEventListeners()
+  addGameBoardEventListeners()
+  addGameConfigEventListeners()
+}
+
+// Función que crea los eventos de las estadísticas del juego
+function addGameStatEventListeners() {
+  // Ver estadísticas y volver al juego
+  document
+    .querySelectorAll(`#${STAT_LABELS.view.id}, #${STAT_LABELS.back.id}`)
+    .forEach((elem) => {
+      elem.addEventListener('click', function () {
+        // Se actualiza el contenido de las tablas de las estadísticas del juego en el caso de que el local storage sea eliminado manualmente
+        if (elem.id === STAT_LABELS.view.id) {
+          printStats()
+        }
+
+        document
+          .querySelectorAll(
+            `#${STAT_LABELS.view.id}, #${STAT_LABELS.back.id}, #${STAT_LABELS.del.id}, .game > section`
+          )
+          .forEach((elem) => {
+            elem.classList.toggle('oculto')
+          })
+      })
+    })
+
+  // Eliminar estadísticas del juego
+  document
+    .getElementById(STAT_LABELS.del.id)
+    .addEventListener('click', function () {
+      showConfirmDialog(
+        '¿Deseas eliminar las estadísticas almacenadas de las partidas?',
+        doStatDeleteActions
+      )
+    })
+}
+
+// Función que realiza las acciones tras confirmar la eliminación de las estadísticas almacenadas de las partidas
+function doStatDeleteActions() {
+  deleteStats()
+  printStats()
+}
+
+// Función que crea los eventos del tablero de juego
+function addGameBoardEventListeners() {
+  document.querySelectorAll('.board > div > div').forEach((color) => {
+    color.addEventListener('click', function () {
+      if (!isStartedGame) {
+        showAlertDialog('Inicia una nueva partida')
+      } else if (!isActiveSequence) {
+        if (
+          !isCorrectColor(
+            color.className.replace(ACTIVE_CLASS_PREFIX, ''),
+            playerSequencePos
+          )
+        ) {
+          endGame()
+        } else {
+          // El jugador no ha terminado de repetir la reproducción de la secuencia de colores
+          if (playerSequencePos !== colorSequencePos) {
+            playerSequencePos++
+          } else {
+            gameHits++
+
+            // El jugador ha acertado la secuencia de colores completa
+            if (playerSequencePos === MAX_SEQ - 1) {
+              endGame()
+            } else {
+              // Se reproduce la secuencia de colores anterior añadiendo un color más
+              colorSequencePos++
+              playerSequencePos = 0
+
+              playColorSequence(colorSequencePos)
+            }
+          }
+        }
+      }
+    })
+  })
+}
+
+// Función que crea los eventos de la configuración del juego
+function addGameConfigEventListeners() {
+  // Nueva partida
+  document
+    .querySelector('.btn[type="submit"]')
+    .addEventListener('click', function () {
+      if (isStartedGame) {
+        showConfirmDialog(
+          'Ya has iniciado una partida. ¿Deseas reiniciar el juego? No se guardará la información sobre la partida en curso',
+          resetGame
+        )
+      } else {
+        initGameSettings()
+        playColorSequence(colorSequencePos)
+      }
+    })
+}
+
 /* Estadísticas del juego */
+
+// Función que rellena el contenido de las tablas de las estadísticas del juego
+function printStats() {
+  for (const difficulty in DIFFICULTIES) {
+    const localStorageStats = getStatsByProperty(DIFF_ID, difficulty)
+    const tbody = document.querySelector(`#${DIFF_ID}-${difficulty} tbody`)
+
+    // Número máximo de aciertos obtenidos en cada dificultad del juego
+    let maxHits = 0
+
+    if (localStorageStats === null || localStorageStats.length === 0) {
+      tbody.innerHTML = getNoStatsRow(difficulty[0])
+    } else {
+      maxHits = getMax(localStorageStats.map((stat) => stat.hits))
+
+      tbody.innerHTML = ''
+
+      // Se listan las estadísticas del juego en orden descendente de fecha
+      for (const stat of localStorageStats.reverse()) {
+        // Se marcan con un color diferente las filas que registran el número máximo de aciertos obtenidos en cada dificultad del juego
+        tbody.innerHTML += `<tr${
+          stat.hits === maxHits ? ' class="highlighted"' : ''
+        }><td headers="${STAT_HEADERS.date.id}">${stat.date}</td><td headers="${
+          STAT_HEADERS.hits.id
+        }">${stat.hits}</td></tr>`
+      }
+    }
+
+    document.querySelector(
+      `#${DIFF_ID}-${difficulty} caption > span`
+    ).innerHTML = `${HIT_MESSAGE}: ${maxHits != 0 ? maxHits : '--'}`
+  }
+}
 
 /* Juego */
 
+// Función que inicia una nueva partida
+function initGameSettings() {
+  gameDifficulty = document.querySelector(
+    `input[name="${DIFF_ID}"]:checked`
+  ).value
+
+  document.getElementById('submit').childNodes.forEach((child) => {
+    child.innerHTML = 'Reiniciar juego'
+  })
+
+  createColorSequence(MAX_SEQ)
+
+  isStartedGame = true
+}
+
+/**
+ * Función que reproduce la secuencia de colores de la partida hasta la posición actual
+ * @param {Number} pos Posición que indica hasta dónde repetir la secuencia de colores de la partida
+ */
+function playColorSequence(pos) {
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+  const playColorSequenceAsync = async () => {
+    try {
+      activeSequenceToggle(false)
+      printMessage('Reproduciendo secuencia de colores...')
+
+      // Tiempo de espera (milisegundos) para reproducir la secuencia de colores de la partida
+      await delay(500)
+
+      for (let index = 0; index <= pos; index++) {
+        const color = document.querySelector(`.${colorSequence[index]}`)
+
+        // Tiempo de espera (milisegundos) para activar el siguiente color de la secuencia
+        await delay(DIFFICULTIES[gameDifficulty].delay)
+        color.style.backgroundColor =
+          BOARD_COLORS[colorSequence[index]].activeColor
+        // Tiempo de espera (milisegundos) para desactivar el color actual de la secuencia
+        await delay(DIFFICULTIES[gameDifficulty].delay)
+        color.style.backgroundColor = BOARD_COLORS[colorSequence[index]].color
+      }
+
+      // Tiempo de espera (milisegundos) para indicar al jugador que repita la secuencia de colores
+      await delay(500)
+
+      printMessage('Repite la secuencia de colores')
+      activeSequenceToggle(true)
+    } catch (error) {
+      console.log(
+        'Se ha producido una interrupción durante la reproducción de la secuencia de colores'
+      )
+      resetGame()
+    }
+  }
+
+  playColorSequenceAsync()
+}
+
+/**
+ * Función que alterna entre la reproducción de la secuencia de colores y que el jugador repita dicha secuencia
+ * @param {Boolean} isPlayerSequence Indica si el jugador tiene que repetir la secuencia de colores
+ */
+function activeSequenceToggle(isPlayerSequence) {
+  isActiveSequence = !isActiveSequence
+
+  document.querySelectorAll('.board > div > div').forEach((color) => {
+    const className = color.className
+
+    // Se eliminan los estilos añadidos durante la reproducción de la secuencia de colores
+    color.removeAttribute('style')
+
+    // Si se reproduce la secuencia de colores se desactivan los pulsadores del tablero de juego
+    if (!isPlayerSequence) {
+      color.className = className.replace(ACTIVE_CLASS_PREFIX, '')
+    } else {
+      // if (!className.includes(ACTIVE_CLASS_PREFIX)) {
+      color.className = `${ACTIVE_CLASS_PREFIX}${className}`
+      // }
+    }
+  })
+}
+
+// Función que finaliza una partida
+function endGame() {
+  showAlertDialogWithActions(
+    `${
+      gameHits === MAX_SEQ
+        ? '¡Enhorabuena!, has acertado la secuencia de colores completa'
+        : 'Has fallado'
+    }. ${HIT_MESSAGE}: ${gameHits}`,
+    resetGame
+  )
+  saveStats(gameDifficulty, gameHits)
+  printStats()
+}
+
+// Función que reinicia el juego
+function resetGame() {
+  isActiveSequence = isStartedGame = false
+
+  // Reinicio de la configuración del juego
+  gameDifficulty = document.querySelector(
+    `input[name="${DIFF_ID}"]:checked`
+  ).value
+
+  document.getElementById('submit').childNodes.forEach((child) => {
+    child.innerHTML = NEW_GAME_LABEL
+  })
+
+  // Reinicio del tablero de juego
+  resetColorSequence()
+
+  gameHits = playerSequencePos = colorSequencePos = 0
+
+  // Reinicio de los mensajes del juego
+  printMessage(NOT_STARTED_GAME_MSG)
+}
+
 /* Util functions */
+
+/**
+ * Función que devuelve el mayor elemento de un array de números
+ * @param {Number[]} array Array de números
+ * @returns Mayor número
+ */
+function getMax(array) {
+  return array.sort((elem1, elem2) => elem2 - elem1)[0]
+}
 
 /* Mensajes emergentes de SweetAlert2 */
 
